39deca6c7cbddd827d90cbd0412b2dd8a29688f6
denid <denisdubov88@gmail.com>
2019-09-17 09:08:00 +0300

    Process study

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,16 @@
 const fs = require('fs')
+const chalk = require('chalk')
 
-fs.appendFileSync('notes.txt', ' I live in Philadelphia')
+let msg = chalk.blue.inverse('Start app')
+console.log(msg)
+
+let success = chalk.green.inverse('Success')
+console.log(success)
+
+let error = chalk.red.inverse('One error')
+console.log(error)
+
+console.log(process.argv)
+//fs.appendFileSync('notes.txt', ' I live in Philadelphia')
 
 //fs.writeFileSync('notes.txt', 'This files created by Node.js');
