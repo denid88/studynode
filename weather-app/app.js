72ad8dbb9bd325c4ed3a07e1fc4b2a6ae9171e15
denid <denisdubov88@gmail.com>
2019-09-26 21:46:29 +0300

    Handle error

--- weather-app/app.js
+++ weather-app/app.js
@@ -8,7 +8,10 @@ request({url: url, json: true}, (err, res) => {
 const getcodeUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiZGVuaWQiLCJhIjoiY2swejdqZjVnMG1vZjNtcW1kNWdiY2J5NSJ9.N8DH_geYvXRl-GYW2pc8Ng'
 
 request({url: getcodeUrl, json: true}, (err, res) => {
-
+  if (err !== null) {
+    console.log(err)
+    return 
+  }
   const latitude = res.body.features[0].center[1]
   const longitude = res.body.features[0].center[0]
   console.log(latitude, longitude)
