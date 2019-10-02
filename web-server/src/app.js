51e89ac3cb9431bb8fee457a2f816604e7dbfe5c
denid <denisdubov88@gmail.com>
2019-10-02 14:16:01 +0300

    Added templates

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,21 +4,33 @@ const express = require('express');
 const app = express();
 
 console.log(__dirname);
-
+//Define path to express config
 const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates');
+
+//Setup handlebars engine  and view location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath)
 
 app.use(express.static(publicDirectoryPath));
 
-app.get('/help', (req, res) => {
-  res.send({
-    name: 'Andrew',
-    age: 18
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather App',
+    name: 'Andrew Jason'
   });
-})
-
+});
 app.get('/about', (req, res) => {
-  res.send('About page!');
-})
+  res.render('about', {
+    title: 'About',
+  });
+});
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: 'Help',
+    text: 'Lorem ipsum',
+  });
+});
 
 app.get('/weather', (req, res) => {
   res.send('Weather page!');
