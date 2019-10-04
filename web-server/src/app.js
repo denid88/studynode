a060cb3ca99e8a39d836a92c37eee230f0151604
denid <denisdubov88@gmail.com>
2019-10-04 23:10:06 +0300

    Added partials

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,16 +1,18 @@
 const path = require('path');
 const express = require('express');
-
+const hbs = require('hbs');
 const app = express();
 
 console.log(__dirname);
 //Define path to express config
 const publicDirectoryPath = path.join(__dirname, '../public');
-const viewsPath = path.join(__dirname, '../templates');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 //Setup handlebars engine  and view location
 app.set('view engine', 'hbs');
-app.set('views', viewsPath)
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
 
 app.use(express.static(publicDirectoryPath));
 
@@ -33,8 +35,35 @@ app.get('/help', (req, res) => {
 });
 
 app.get('/weather', (req, res) => {
-  res.send('Weather page!');
-})
+  if (!req.query.address) {
+    return res.send({
+      error: 'You must provided address'
+    })
+  }
+  
+  res.send({
+    forecast: 'It`s showing',
+    location: 'Philadelphia',
+    address: req.query.address
+  });
+
+});
+
+app.get('/products', (req,res) => {
+  console.log(req.query)
+  if (!req.query.search) {
+    return res.send({
+      error: 'Your must search provided'
+    })
+  }
+  res.send({
+    products: []
+  })
+});
+
+app.get('*', (req, res) => {
+  res.render('404');
+});
 
 app.listen(3030, () => {
   console.log('Server up! on port 3030')
