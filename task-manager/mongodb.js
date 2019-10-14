c02eee0d0b2c5965f811e3e3cc2c26415029ae21
denid <denisdubov88@gmail.com>
2019-10-14 09:31:26 +0300

    Trying to insert doc

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,10 +1,13 @@
-const mongodb = require('mongodb');
-const mongoClient = mongodb.MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+const id = new ObjectID();
+console.log(id);
+console.log(id.getTimestamp());
 
 const connectionUrl = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
 
-mongoClient.connect(connectionUrl, {
+MongoClient.connect(connectionUrl, {
   useUnifiedTopology: true,
   useNewUrlParser: true
 }, async (error, client) => {
@@ -13,9 +16,32 @@ mongoClient.connect(connectionUrl, {
   }
   console.log('Connected correctly!');
   const db = client.db(databaseName);
+  
+  
   db.collection('users').insertOne({
+    _id: id,
     name: 'Denid',
     age: 32
+  }, (error, result) => {
+    if (error) () => console.log('Unable to insert user');
+
+    console.log(result.ops);
   });
+  
+ /*
+ db.collection('users').insertMany([
+  {
+    description: 'Learn node js',
+    completed: false
+  },
+  {
+    description: 'Learn react js',
+    completed: true
+  },
+  ], (error, result) => {
+  if (error) () => console.log('Unable to insert user');
+  
+  console.log(result.ops);
+})*/
 
 });
