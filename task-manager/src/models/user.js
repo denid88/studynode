02b6e6b680a3b4ef915f6f219a0bb4e62ca89ab8
denid <denisdubov88@gmail.com>
2019-11-07 00:21:14 +0200

    Added in project jst

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -18,7 +19,13 @@ const userSchema = mongoose.Schema({
                 throw new Error('Wrong email!');
             }
         }
-    }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        },
+    }]
 });
 
 userSchema.statics.findByCredentionals = async (email, password) => {
@@ -34,6 +41,14 @@ userSchema.statics.findByCredentionals = async (email, password) => {
     }
 
     return user;
+};
+
+userSchema.methods.generateAuthToken = async function () {
+    const user = this;
+    const token = jwt.sign({ _id: user._id.toString()}, 'thismyapp');
+    user.tokens = user.tokens.concat({ token });
+    await user.save();
+    return token;
 };
 
 userSchema.pre('save', async function (next) {
--- task-manager/src/routers/users.js
+++ task-manager/src/routers/users.js
@@ -6,7 +6,8 @@ router.post('/users', async (req, res) => {
   const user = new User(req.body);
   try {
       await user.save();
-      res.status(201).send(user);
+      const token = await user.generateAuthToken();
+      res.status(201).send({user, token});
   } catch (e) {
       res.status(400).send(e);
   }
@@ -71,7 +72,9 @@ router.delete('/users/:id', async (req, res) => {
 router.post('/users/login', async (req, res) => {
   try {
     const user = await User.findByCredentionals(req.body.email, req.body.password);
-    res.status(200).send(user);
+    console.log(user);
+    const token = await user.generateAuthToken();
+    res.status(200).send({user, token});
   } catch (e) {
       res.status(400).send(e);
   }
