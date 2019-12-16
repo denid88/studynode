86c3a149c91c683e66b44251343084ba25ec6b95
denid <denisdubov88@gmail.com>
2019-12-16 19:41:12 +0200

    Chat app socket.io

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -1,13 +1,21 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
 const app = express();
+const socketio = require('socket.io');
 
 const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+const io = socketio(server);
 
 const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-app.listen(port, () => {
+io.on('connection', () => {
+  console.log('connection perfect!');
+});
+
+server.listen(port, () => {
   console.log(`Starting server on port: ${port}`);
 });
