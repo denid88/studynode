9d5c06671d271bf1daea089bcee52439fe7ea6a9
denid <denisdubov88@gmail.com>
2019-12-17 20:24:53 +0200

    Chat app socket added

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,1 +1,33 @@
-io();
+const socket = io();
+
+socket.on('message', function(message) {
+    console.log(message);
+});
+
+document.querySelector('#message-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const message = e.target.elements.message.value;
+    socket.emit('sendMessage', message, function(error){
+
+        if(error) {
+            return console.log(error);
+        }
+        console.log('this message was delivered!');
+    });
+});
+
+document.querySelector('#location').addEventListener('click', function(e) {
+    e.preventDefault();
+    if (!navigator.geolocation) {
+        return alert('Geo not found!');
+    }
+
+    navigator.geolocation.getCurrentPosition(function(position) {
+        socket.emit('setGeoposition', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        }, function(){
+            console.log('Location shared!');
+        });
+    })
+});
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -4,17 +4,46 @@ const express = require('express');
 const app = express();
 const socketio = require('socket.io');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3030;
 const server = http.createServer(app);
 const io = socketio(server);
+
+const Filter = require('bad-words');
 
 const publicDirectoryPath = path.join(__dirname, '../public');
 
 app.use(express.static(publicDirectoryPath));
 
-io.on('connection', () => {
+let count = 0;
+
+io.on('connection', (socket) => {
   console.log('connection perfect!');
+
+  socket.emit('message', 'Welcome!');
+  socket.broadcast.emit('message', 'A new user has joined');
+
+  socket.on('sendMessage', function(message, callback) {
+    const filter = new Filter();
+
+    if(filter.isProfane(message)) {
+      return callback('Profanity is no allow');
+    }
+
+    io.emit('message', message);
+    callback();
+  });
+
+  socket.on('setGeoposition', function(coords, callback) {
+    io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`);
+    callback();
+  });
+
+  socket.on('disconnect', () => {
+    console.log('disconnecting');
+    io.emit('message', 'A user have left!');
+  });
 });
+
 
 server.listen(port, () => {
   console.log(`Starting server on port: ${port}`);
